b62512ed974c9d96a504e3ee0b4ab91f6b41b332
Travis Alstrand <92395688+TravisAlstrand@users.noreply.github.com>
2025-02-11 14:03:30 -0800

    Desktop navigation implemented

--- src/Sections/Header.jsx
+++ src/Sections/Header.jsx
@@ -29,42 +29,57 @@ const Header = () => {
   }, [menuOpen]);
 
   return (
-    <header className="flex h-8 bg-slate-800 p-2 text-indigo-100">
-      <div className="flex w-1/2 items-center">
-        <span aria-hidden="true">&lt;</span>
-        <span className="mr-2 italic">Travis Alstrand</span>
-        <span aria-hidden="true">/&gt;</span>
+    <header className="h-12 bg-slate-800 p-2 text-indigo-100 md:text-xl lg:h-16 lg:p-4">
+      <div className="mx-auto flex h-full max-w-[1440px]">
+        <div className="flex w-1/2 items-center">
+          <span aria-hidden="true">&lt;</span>
+          <span className="mr-2 italic">Travis Alstrand</span>
+          <span aria-hidden="true">/&gt;</span>
+        </div>
+        <div className="flex w-1/2 items-center justify-end">
+          {/* Mobile Nav Button */}
+          <button
+            className="hover:cursor-pointer lg:hidden"
+            aria-label={
+              menuOpen ? "Close navigation menu" : "Open navigation menu"
+            }
+            aria-controls="mobile-navigation"
+            aria-expanded={menuOpen}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            {menuOpen ? (
+              <FiX className="text-2xl md:text-3xl" aria-hidden="true" />
+            ) : (
+              <FiMenu className="text-2xl md:text-3xl" aria-hidden="true" />
+            )}
+          </button>
+          {/* Desktop Nav */}
+          <nav
+            className="hidden w-full justify-end gap-8 lg:flex"
+            role="navigation"
+          >
+            <a href="#about">About Me</a>
+            <a href="#projects">My Projects</a>
+            <a href="#contact">Contact Me</a>
+          </nav>
+        </div>
+        {/* Mobile Nav Menu */}
+        {menuOpen ? (
+          <nav
+            id="mobile-navigation"
+            role="navigation"
+            className="fixed top-8 left-0 flex h-12 w-full items-center justify-evenly bg-slate-800 lg:hidden"
+          >
+            <a href="#about" ref={firstLinkRef}>
+              About Me
+            </a>
+            <a href="#projects">My Projects</a>
+            <a href="#contact">Contact Me</a>
+          </nav>
+        ) : (
+          <></>
+        )}
       </div>
-      <div className="flex w-1/2 items-center justify-end">
-        <button
-          className="hover:cursor-pointer"
-          aria-label={
-            menuOpen ? "Close navigation menu" : "Open navigation menu"
-          }
-          aria-expanded={menuOpen}
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          {menuOpen ? (
-            <FiX className="text-2xl" aria-hidden="true" />
-          ) : (
-            <FiMenu className="text-2xl" aria-hidden="true" />
-          )}
-        </button>
-      </div>
-      {menuOpen ? (
-        <nav
-          role="navigation"
-          className="fixed top-8 left-0 flex h-8 w-full items-center justify-evenly bg-slate-800"
-        >
-          <a href="#about" ref={firstLinkRef}>
-            About Me
-          </a>
-          <a href="#projects">My Projects</a>
-          <a href="#contact">Contact Me</a>
-        </nav>
-      ) : (
-        <></>
-      )}
     </header>
   );
 };
